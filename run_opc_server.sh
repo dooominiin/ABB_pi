6db99cb76795590e1e4ce4564fc87c3e8eee5194
Dooominiin <ydominique.torti@gmx.ch>
2023-05-15 13:32:53 +0200

    versuch, opc_server automatisch laufen zu lassen

---
+++ run_opc_server.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while true
+do
+    # Starten des Python-Skripts
+    /usr/bin/python3 /home/mister/Desktop/ABB_Projekt/OPC/opc_test_server.py
+
+    # Überprüfen, ob das Skript noch läuft
+    if [ $? -eq 0 ]; then
+        echo "Skript wird ausgeführt."
+    else
+        echo "Skript wurde beendet. Starte neu..."
+    fi
+
+    # Warten für eine gewisse Zeit, bevor das Skript erneut gestartet wird
+    sleep 1
+done
