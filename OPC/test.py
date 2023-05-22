e8eb7cbf8f510f1b492f0664cd7aefa9ad56b60c
dominique torti <dominique.torti@gmx.ch>
2023-05-22 23:47:54 +0200

    a

--- OPC/test.py
+++ OPC/test.py
@@ -1,7 +1,8 @@
 from opcua import Client
 
 # Verbindung zum OPC-UA-Server herstellen
-url = "opc.tcp://192.168.43.1:5000/freeopcua/server/"
+url = "opc.tcp://192.168.43.1:4840/freeopcua/server/"
+#url = "opc.tcp://localhost:4840/freeopcua/server/"
 client = Client(url)
 
 # Verbindung herstellen und Verbindungsstatus überprüfen
