6d52d7aaf098a5bfc0ac3af4944cf679b0173497
Dooominiin <ydominique.torti@gmx.ch>
2023-05-25 20:11:56 +0200

    a

--- OPC/opcua_client.py
+++ OPC/opcua_client.py
@@ -1,6 +1,7 @@
 from threading import Thread
 import time
-from opcua import Client
+from opcua import Client, ua
+import json
 
 class SubHandler(object):
 
@@ -27,12 +28,14 @@ class OpcUaClient:
     def __init__(self, dt, regler):
         self.client = Client("opc.tcp://localhost:4840/freeopcua/server/")
         self.client = Client("opc.tcp://192.168.43.97:4840/freeopcua/server/")  # adresse lenovo hotspot
+        self.terminate = False
         try:
             self.client.connect()
             root = self.client.get_root_node()
         except:
             print("Verbindung zum OPC Server nicht möglich!")
-        self.terminate = False
+            self.terminate = False
+
         self.thread = Thread(target=self.loop_forever)
         self.output = 0
         self.dt = dt # diskretisierungszeitschritt
@@ -47,13 +50,23 @@ class OpcUaClient:
             self.handler.get_regler(self.regler)
             self.subscription = self.client.create_subscription(500, self.handler) 
             
-            # meine variabeln
-            variable_names = ["T_D40", "T_tank", "T_kuehl", "F", "s", "r"]
-            for var_name in variable_names:
-                myvar = root.get_child(["0:Objects", "2:Temperaturen",f"2:{var_name}"])
-                self.handle = self.subscription.subscribe_data_change(myvar)
-        except:
+            # Lade die Variablen aus der JSON-Datei und erstelle sie im OPC-Server
+            with open("OPC/variablen.json", "r") as file:
+                variables = json.load(file)
+                for var_info in variables:
+                    name = var_info["name"]
+                    namespace = var_info["namespace"]
+                    string = var_info["string"]
+
+                    if var_info["subscribe"]:
+                        node=self.client.get_node(nodeid=f"{namespace};{string}")
+                        print(node)
+                        #myvar = root.get_child(f"{namespace};{string}")
+                        #self.handle = self.subscription.subscribe_data_change(node_id)
+        except Exception as e:
             print("subscribe der Variabeln nicht möglich!")
+            print(e)
+            self.terminate = True
 
     
     def set_output(self,output):
