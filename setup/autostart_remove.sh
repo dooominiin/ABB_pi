680b8cf8dde1b576f7549262d23d1a18d623f08b
Dooominiin <ydominique.torti@gmx.ch>
2023-05-30 19:46:09 +0200

    a

--- setup/autostart_remove.sh
+++ setup/autostart_remove.sh
@@ -2,7 +2,7 @@
 # entfernt den autostarteintrag für den Regler
 
 # Überprüfe, ob die Zeile vorhanden ist
-if grep -q "@lxterminal -e python3 /home/mister/Desktop/ABB_pi/main.py" /etc/xdg/lxsession/LXDE-pi/autostart; then
+if grep -q "@lxterminal -e python3 /home/mister/Desktop/ABB_Projekt/main.py" /etc/xdg/lxsession/LXDE-pi/autostart; then
   # Entferne die Zeile aus der Datei
   sudo sed -i '/@lxterminal -e python3 \/home\/mister\/Desktop\/ABB_pi\/main.py/d' /etc/xdg/lxsession/LXDE-pi/autostart
   echo "Der Autostart-Eintrag wurde erfolgreich entfernt."
--- setup/autostart_setup.sh
+++ setup/autostart_setup.sh
@@ -2,9 +2,9 @@
 # fügt einen autostarteintrag für den Regler hinzu
 
 # Überprüfe, ob die Zeile bereits vorhanden ist
-if ! grep -q "@lxterminal -e python3 /home/mister/Desktop/ABB_pi/main.py" /etc/xdg/lxsession/LXDE-pi/autostart; then
+if ! grep -q "@lxterminal -e python3 /home/mister/Desktop/ABB_Projekt/main.py" /etc/xdg/lxsession/LXDE-pi/autostart; then
   # Füge die Zeile am Ende der Datei hinzu
-  echo "@lxterminal -e python3 /home/mister/Desktop/ABB_pi/main.py" | sudo tee -a /etc/xdg/lxsession/LXDE-pi/autostart > /dev/null
+  echo "@lxterminal -e python3 /home/mister/Desktop/ABB_Projekt/main.py" | sudo tee -a /etc/xdg/lxsession/LXDE-pi/autostart > /dev/null
   echo "Die Autostart Zeile wurde erfolgreich hinzugefügt."
   
   # Führe einen Neustart des Raspberry Pi aus
