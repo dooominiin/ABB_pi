fcae660f03129f5768ce2a98f347f42a6a1ae957
dominique torti <dominique.torti@gmx.ch>
2023-08-08 12:02:56 +0200

    vorsicht, vielleicht  monitorserver verändert

---
+++ Monitor/test.m
@@ -0,0 +1,4 @@
+pyenv('Version',"C:\Program Files\Python310\python.exe");
+pyenv
+py.print("w<dw");
+
---
+++ OPC/opcua_client_monitor.py
@@ -0,0 +1,132 @@
+from threading import Thread
+import time
+from opcua import Client, ua
+import json
+
+# Der OPCUA-Client stellt die verbindung zum Leitsystem dar und handelt 
+# INPUT und OUTPUT des Reglers. Das OPCUA Protokoll stellt eine Public 
+# Subscriber Struktur zur verfügung. Das bedeuted, dass der CLient beim 
+# Leitsystem bei den Sensorwerten "subscribed" und dann benachrichtigt 
+# wird, wenn neue Werte vorliegen. Die Variabeln zum werden über das 
+# variabeln.json File verwaltet. 
+
+class SubHandler(object):
+
+    """
+    Subscription Handler. To receive events from server for a subscription
+    data_change and event methods are called directly from receiving thread.
+    Do not do expensive, slow or network operation there. Create another 
+    thread if you need to do such a thing
+    """
+    def get_monitor(self,monitor):
+        self.monitor = monitor
+
+    def datachange_notification(self, node, val, data):
+        #print("Python: New data change event", node, val)
+        #self.monitor.set_input(val,node) # neue daten zum monitorprogramm schicken
+        print("subscribed variabel {} wurde getriggert".format(node))
+
+    def event_notification(self, event):
+        print("Python: New event", event)
+
+
+
+class OpcUaClient:
+    def __init__(self, dt):
+        self.dt = dt # diskretisierungszeitschritt
+        self.zähler = 0                
+        self.client = Client("opc.tcp://192.168.43.203:4841/freeopcua/server/")  # adresse lenovo handy hotspot
+        self.terminate = False
+        self.running = True
+        zähler = 0
+        while not self.terminate:
+            try:
+                self.client.connect()
+                print("Verbindung zum OPC Server erfolgreich")
+                break
+            except:
+                print(f"Verbindung zum OPC Server nicht möglich! {zähler+1}er Versuch!")
+                zähler += 1
+                if zähler >= 5:
+                    print(f"Abbruch nach {zähler} Versuchen!")
+                    self.terminate = True
+                    self.running = False
+
+        if not self.terminate:
+            # subscribing to a variable node
+            self.handler = SubHandler()
+            #self.handler.get_regler(self.regler)
+            self.subscription = self.client.create_subscription(500, self.handler) 
+                
+            # Lade die Variablen aus der JSON-Datei und erstelle sie im OPC-Server
+            with open("OPC/variablen_monitoring.json", "r", encoding='utf-8') as file:
+                variables = json.load(file)
+                for var_info in variables:
+                    name = var_info["name"]
+                    namespace = var_info["namespace"]
+                    string = "{}{}".format(var_info["string"],name)
+                    if var_info["subscribe"]:
+                        try:            
+                            node=self.client.get_node(nodeid=f"{namespace};{string}")
+                            self.handle = self.subscription.subscribe_data_change(node)
+                            print("subscribed to: ",name)
+
+                        except Exception as e:
+                            print("subscribe der Variabeln nicht möglich!")
+                            print(e)
+                            self.terminate = True
+
+    
+    def set_output(self,output):
+        if not self.am_senden:
+            self.output = output
+         
+    def send(self):
+        self.zähler += self.dt
+        if self.zähler >= self.output_update_intervall:
+            t1 = time.time()
+            with open("OPC/variablen_monitoring.json", "r", encoding='utf-8') as file:
+                variables = json.load(file)
+            self.am_senden = True
+            for var_info in variables:
+                name = var_info["name"]
+                namespace = var_info["namespace"]
+                string = var_info["string"]
+                if var_info["is_output"]:
+                    #print("update output",name,float(self.output[name]))
+                    time_1=time.time()
+                    self.client.get_node(f"{namespace};{string}").set_value(float(self.output[name]))
+            t2 = time.time()
+            #print("output wurde gesendet in {:.4f} s".format(t2-t1))
+            self.am_senden = False
+            self.zähler = 0
+
+        
+
+    def loop_start(self):
+        if not self.terminate:
+            self.thread.start()
+
+    def loop_forever(self):
+        while not self.terminate:                       
+            start_time = time.time()  # Startzeit speichern
+            self.send()
+            elapsed_time = time.time() - start_time  # Zeit seit Start speichern
+            time.sleep(max(0, self.dt - elapsed_time))  # Schlafzeit berechnen und warten
+  
+    def loop_stop(self):
+        self.terminate = True
+        try:
+            self.thread.join()
+            if hasattr(self.client, 'subscription'):
+                self.client.subscription.delete()
+            self.client.disconnect()
+        except:
+            pass
+        self.running = False
+
+    def is_running(self):
+        return self.running
+    
+if __name__ == "__main__":
+    c = OpcUaClient(0.1)
